dee08fa4f92afcfb35a5ccfb6a8bfebe9e0c4074
Chenyang WANG <38653627+fighttiger25@users.noreply.github.com>
2023-01-26 11:28:40 +0100

    Initial Commit

---
+++ multitouch.demo/multitouch_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  multitouch_demoApp.swift
+//  multitouch.demo
+//
+//  Created by Chen on 26.01.23.
+//
+
+import SwiftUI
+
+@main
+struct multitouch_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
